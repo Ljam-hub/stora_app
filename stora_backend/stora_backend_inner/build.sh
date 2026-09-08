1eabb23ab889050b10159464474bad2e1b3e25f5
lejon osial <osiallj@gmail.com>
2026-09-09 01:44:03 +0800

    Fix build/start scripts and database config

--- stora_backend/stora_backend_inner/build.sh
+++ stora_backend/stora_backend_inner/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-# Render build script — runs on every deploy.
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
-
 python manage.py migrate
